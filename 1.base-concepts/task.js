a15aff89304bfc5cd0d9bd758abafd454d26636c
sidawast <sidawast@mail.ru>
2022-06-03 21:26:16 +0300

    add ed code

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,15 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+  if(d == 0) {
+    arr.push(- b / (2 * a));
+  }
+  if(d > 0) {
+    arr.push(- b - Math.sqrt(d) / (2 * a)); 
+    arr.push(- b + Math.sqrt(d) / (2 * a));
+  }
+ // else if(d < 0) {}
   return arr; // array
 }
 
