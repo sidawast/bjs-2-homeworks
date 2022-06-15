a212527f8f4c38dddbf47886df86b70145c076e0
анатолий <sidawast@mail.ru>
2022-06-15 19:14:43 +0300

    FIXED code

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -9,7 +9,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  resultArr = arr.filter((item) => item > 0 && item % 3 == 0).map(item => item);
+  resultArr = arr.filter((item) => item > 0 && item % 3 == 0).map(item => item*10);
 
   return resultArr; // array
 }
